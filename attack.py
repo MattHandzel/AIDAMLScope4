2657e4cb2c213faf7dd31c471f1fa0623918606e
MattHandzel <matthewwalkosz@gmail.com>
2025-04-12 22:22:35 -0500

    yay, works

--- attack.py
+++ attack.py
@@ -5,19 +5,19 @@ import numpy as np
 
 # TODO: Participants implement their attack here!
 def create_adversarial_example(model, image, true_label, epsilon=0.1):
-    """
-    Inputs:
-    - model: Pretrained model
-    - image: Original image (28, 28) numpy array
-    - true_label: 0 (happy), 1 (sad), 2 (neutral)
-    - epsilon: Perturbation strength
+    image_tensor = torch.tensor(image, dtype=torch.float32).unsqueeze(0).unsqueeze(0)
+    image_tensor.requires_grad = True
 
-    Returns:
-    - perturbed_image: Adversarial example (28, 28) numpy array
-    """
+    output = model(image_tensor)
+    loss = torch.nn.functional.cross_entropy(output, torch.tensor([true_label]))
 
-    # TODO: Create an adversarial example
-    perturbed_image = ...
+    model.zero_grad()
+    loss.backward()
+
+    # FGSM attack
+    perturbation = epsilon * image_tensor.grad.data.sign()
+    perturbed_image = image_tensor + perturbation
+    perturbed_image = torch.clamp(perturbed_image, 0, 1)
 
     return perturbed_image.detach().numpy().squeeze()
 
@@ -27,8 +27,8 @@ def test_attack():
     emoji = generate_emoji("happy")
     perturbed = create_adversarial_example(model, emoji, 0)
 
-    orig_pred = model(torch.tensor(emoji).unsqueeze(0)).argmax().item()
-    adv_pred = model(torch.tensor(perturbed).unsqueeze(0)).argmax().item()
+    orig_pred = model(torch.tensor(emoji).unsqueeze(0).unsqueeze(0)).argmax().item()
+    adv_pred = model(torch.tensor(perturbed).unsqueeze(0).unsqueeze(0)).argmax().item()
 
     visualize_attack(emoji, perturbed, perturbed - emoji)
     stats = calculate_stats(emoji, perturbed, 0, adv_pred)
